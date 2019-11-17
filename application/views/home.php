ed5ad09ee2d75903bd6d703d3c6290283aef6480
Dagus Wijaya <dagus.wijaya@gmail.com>
2019-11-17 17:53:20 +0700

    First commit

---
+++ application/views/home.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Luncur-in aja</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css" >
+    <!-- Icon -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/fonts/line-icons.css">
+    <!-- Owl carousel -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/owl.theme.css">
+    
+    <!-- Animate -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/animate.css">
+    <!-- Main Style -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/main.css">
+    <!-- Responsive Style -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/responsive.css">
+    <style>
+    @import url('https://fonts.googleapis.com/css?family=Catamaran:800&display=swap');
+    </style> 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-146706044-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-146706044-1');
+    </script>
+
+  </head>
+  <body>
+
+    <!-- Header Area wrapper Starts -->
+    <header id="header-wrap">
+      <!-- Navbar Start -->
+      <nav class="navbar navbar-expand-md bg-inverse fixed-top scrolling-navbar">
+        <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <a href="<?php echo base_url();?>" class="navbar-brand img-fluid"><img src="<?php echo base_url();?>assets/img/luncurin-logo.png" alt="logo luncurin" width="240px" ></a>       
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="lni-menu"></i>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav mr-auto w-100 justify-content-end clearfix">
+              <li class="nav-item active">
+                <a class="nav-link" href="#hero-area">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#hero-area">
+                  Category
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#hero-area">
+                  Login
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <!-- Navbar End -->
+    <!-- Top Product Section Start -->
+    <section id="product" class="section-padding bg-gray hero-area-like-padding">
+      <div class="container">
+        <div class="section-header text-center">          
+          <h2 class="section-title wow fadeInDown" data-wow-delay="0.3s">Awesome Stuff</h2>
+          <div class="shape wow fadeInDown" data-wow-delay="0.3s"></div>
+        </div>
+        <div class="row">
+          <div class="col-lg-6 col-md-12 col-xs-12">
+            <!-- Team Item Starts -->
+            <div class="team-item wow fadeInRight" data-wow-delay="0.2s">
+              <div class="team-img">
+                <img class="img-fluid" src="<?php echo base_url();?>assets/img/team/team-01.png" alt="">
+              </div>
+              <div class="contetn">
+                <div class="info-text">
+                  <h3><a href="#">Your Stuff</a></h3>
+                  <p class="pink">Category</p>
+                </div>
+                <p>This is short description about your stuff.</p>
+                <ul class="social-icons small-top-padding">
+                  <li><a href="#"><i class="lni-search pink" aria-hidden="true"></i></a></li>
+                  <li><a href="#"><i class="lni-star pink"></i></a></li>
+                  <li><a href="#"><i class="lni-bolt-alt pink"></i></a></li>
+                </ul>
+              </div>
+            </div>
+            <!-- Team Item Ends -->
+          </div>
+          <div class="col-lg-6 col-md-12 col-xs-12">
+            <!-- Team Item Starts -->
+            <div class="team-item wow fadeInRight" data-wow-delay="0.2s">
+              <div class="team-img">
+                <img class="img-fluid" src="<?php echo base_url();?>assets/img/team/team-01.png" alt="">
+              </div>
+              <div class="contetn">
+                <div class="info-text">
+                  <h3><a href="#">Your Stuff</a></h3>
+                  <p class="pink">Category</p>
+                </div>
+                <p>This is short description about your stuff.</p>
+                <ul class="social-icons small-top-padding">
+                  <li><a href="#"><i class="lni-search pink" aria-hidden="true"></i></a></li>
+                  <li><a href="#"><i class="lni-star pink"></i></a></li>
+                  <li><a href="#"><i class="lni-bolt-alt pink"></i></a></li>
+                </ul>
+              </div>
+            </div>
+            <!-- Team Item Ends -->
+          </div>
+          <div class="col-lg-6 col-md-12 col-xs-12">
+            <!-- Team Item Starts -->
+            <div class="team-item wow fadeInRight" data-wow-delay="0.2s">
+              <div class="team-img">
+                <img class="img-fluid" src="<?php echo base_url();?>assets/img/team/team-01.png" alt="">
+              </div>
+              <div class="contetn">
+                <div class="info-text">
+                  <h3><a href="#">Your Stuff</a></h3>
+                  <p class="pink">Category</p>
+                </div>
+                <p>This is short description about your stuff.</p>
+                <ul class="social-icons small-top-padding">
+                  <li><a href="#"><i class="lni-search pink" aria-hidden="true"></i></a></li>
+                  <li><a href="#"><i class="lni-star pink"></i></a></li>
+                  <li><a href="#"><i class="lni-bolt-alt pink"></i></a></li>
+                </ul>
+              </div>
+            </div>
+            <!-- Team Item Ends -->
+          </div>
+          <div class="col-lg-6 col-md-12 col-xs-12">
+            <!-- Team Item Starts -->
+            <div class="team-item wow fadeInRight" data-wow-delay="0.2s">
+              <div class="team-img">
+                <img class="img-fluid" src="<?php echo base_url();?>assets/img/team/team-01.png" alt="">
+              </div>
+              <div class="contetn">
+                <div class="info-text">
+                  <h3><a href="#">Your Stuff</a></h3>
+                  <p class="pink">Category</p>
+                </div>
+                <p>This is short description about your stuff.</p>
+                <ul class="social-icons small-top-padding">
+                  <li><a href="#"><i class="lni-search pink" aria-hidden="true"></i></a></li>
+                  <li><a href="#"><i class="lni-star pink"></i></a></li>
+                  <li><a href="#"><i class="lni-bolt-alt pink"></i></a></li>
+                </ul>
+              </div>
+            </div>
+            <!-- Team Item Ends -->
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Team Section End -->
+
+      <!-- Hero Area Start -->
+      <div id="hero-area" class="hero-area-bg">
+        <div class="container">      
+          <div class="row">
+            <div class="col-lg-7 col-md-12 col-sm-12 col-xs-12">
+              <div class="contents"><br>
+                <h2 class="head-title">Saatnya mereka tahu<br>produk digital kamu</h2>
+                <p>Produk yang hebat adalah produk yang diluncurkan, jangan ditunda lagi<br> ajak dunia untuk mencoba karyamu</p>
+                <div class="header-button">
+                  <a href="#" class="btn btn-common">Daftar</i></a>
+                  <a href="#" class="btn btn-border video-popup">Download</i></a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-5 col-md-12 col-sm-12 col-xs-12">
+              <div class="intro-img">
+                <img class="img-fluid" src="<?php echo base_url();?>assets/img/intro-mobile.png" alt="">
+              </div>            
+            </div>
+          </div> 
+        </div> 
+      </div>
+      <!-- Hero Area End -->
+
+    </header>
+    <!-- Header Area wrapper End -->
+
+    <footer>
+      <div id="copyright">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="copyright-content">
+                <p>Copyright © 2019 Luncur.in All Right Reserved</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>   
+    </footer> 
+    <!-- Footer Section End -->
+
+    <!-- Go to Top Link -->
+    <a href="#" class="back-to-top">
+    	<i class="lni-arrow-up"></i>
+    </a>
+    
+    <!-- Preloader -->
+    <div id="preloader">
+      <div class="loader" id="loader-1"></div>
+    </div>
+    <!-- End Preloader -->
+    
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="<?php echo base_url();?>assets/js/jquery-min.js"></script>
+    <script src="<?php echo base_url();?>assets/js/popper.min.js"></script>
+    <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url();?>assets/js/owl.carousel.min.js"></script>
+    <script src="<?php echo base_url();?>assets/js/wow.js"></script>
+    <script src="<?php echo base_url();?>assets/js/jquery.nav.js"></script>
+    <script src="<?php echo base_url();?>assets/js/scrolling-nav.js"></script>
+    <script src="<?php echo base_url();?>assets/js/jquery.easing.min.js"></script>  
+    <script src="<?php echo base_url();?>assets/js/main.js"></script>
+    <script src="<?php echo base_url();?>assets/js/form-validator.min.js"></script>
+    <script src="<?php echo base_url();?>assets/js/contact-form-script.min.js"></script>
+      
+  </body>
+</html>
